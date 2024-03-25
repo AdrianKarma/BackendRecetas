79f2d45444d3c98471267bef48b187cfcf6e44cd
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-03-25 17:55:06 -0300

    puerto configurado

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ import express from "express"
 
 console.log("hola mundo")
 
+//configurar puerto
+const app =express();
+app.set ('port', process.env.PORT || 4000);
+app.listen(app.get('port'), ()=> {console.log('Estoy en el puerto '+app.get('port'))})
 
+
+//configurar middewares
