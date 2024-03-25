34f923ef6375b5b5841ed33caeaba04c3c585432
Adrian Chanampa <adrian.karma.chan@gmail.com>
2024-03-25 18:05:21 -0300

    middlewares agregados

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-import express from "express"
+import express from 'express';
+import 'dotenv/config'; 
+import cors from 'cors';
+import morgan from 'morgan';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import productosRouter from './src/routes/productos.routes.js';
+import './src/database/database.js'
 
+//1  puerto
+const app = express();
 
-console.log("hola mundo")
+app.set('port', process.env.PORT || 4000 );
+app.listen(app.get('port'), ()=>{
+    console.log('Estoy en el puerto '+ app.get('port'))
+})
+//2 middlewares
+app.use(cors()); 
+app.use(morgan('dev'))
+app.use(express.json()); 
+app.use(express.urlencoded({extended:true})) 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-//configurar puerto
-const app =express();
-app.set ('port', process.env.PORT || 4000);
-app.listen(app.get('port'), ()=> {console.log('Estoy en el puerto '+app.get('port'))})
+app.use(express.static(path.join(__dirname,'/public')))
 
+//3  rutas
 
-//configurar middewares
+app.use('/api', productosRouter )
+
